ccab0ccbba7ed8bb325496427e5998131433b304
vnkrishnan89@gmail.com <vnkrishnan89@gmail.com>
2017-06-30 10:44:50 +0530

    changed log message

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello DevOps");
+		get("/hello", (req, res) -> "Hello Java");
 	}
 }
